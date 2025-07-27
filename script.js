626dfb681165f91270a48342e196fff6f0f67eaa
Abhinav Sharma <abhinavsharma3614@gmail.com>
2025-07-27 22:40:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const display=document.querySelector('#display');
+function sin(){
+    display.value=Math.sin(display.value)
+}
+
+function cos(){
+    display.value=Math.cos(display.value)
+}
+function tan(){
+    display.value=Math.tan(display.value)
+}
+function sqrt(){
+    display.value=Math.sqrt(display.value)
+}
